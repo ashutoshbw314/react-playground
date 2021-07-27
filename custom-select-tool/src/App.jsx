82aa7104d27e3ddcb47ffe033bde5390cae25b2c
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-07-27 20:33:06 +0600

    made Choice stateless

--- custom-select-tool/src/App.jsx
+++ custom-select-tool/src/App.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 const BTC = 'Bitcoin';
 const CREDITCARD = 'Creditcard';
 
+const Choice = ({active, onClick, label}) => {
+  const cssClasses = ['choice'];
+
+  if (active) {
+    cssClasses.push('active');
+  }
+
+  return (
+    <div className={cssClasses.join(' ')} onClick={onClick}>
+      {label}
+    </div>
+  );
+};
+
 class Switch extends React.Component {
   state = {
     paymentMethod: BTC,
@@ -31,9 +45,24 @@ class Switch extends React.Component {
   render() {
     return (
       <div>
-        {this.renderChoice(CREDITCARD)}
-        {this.renderChoice(BTC)}
-        <p>Pay with: {this.state.paymentMethod}</p>
+        {/*
+          {this.renderChoice(CREDITCARD)}
+          {this.renderChoice(BTC)}
+        */}
+
+        {/* Using stateless component instead of the `renderChoice` method */}
+        <Choice
+          onClick={this.select(CREDITCARD)}
+          active={this.state.paymentMethod == CREDITCARD}
+          label="Pay with Creditcard"
+        />
+        <Choice
+          onClick={this.select(BTC)}
+          active={this.state.paymentMethod == BTC}
+          label="Pay with BTC"
+        />
+
+        <p>Paying with: {this.state.paymentMethod}</p>
       </div>
     )
   }
